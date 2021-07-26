c3478ea39c849e2ca573ba159a76ba1b52763246
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-26 09:34:13 -0400

    Cria links para as rotas home, about e users

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/users" component={Users} />
