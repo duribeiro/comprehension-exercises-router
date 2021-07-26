d775c42c2bad1bcd25baea9dee9070d7f52291ee
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-26 11:05:28 -0400

    Passando props pelo router

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { id } = this.props.match.params;
+    const { greetingsMessage } = this.props;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingsMessage }, My awesome Users component, My id is: {id}</p>
       </div>
     );
   }
