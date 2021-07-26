33a129f0fd9c4590e8053a0ea44d97336749ebce
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-26 12:08:51 -0400

    Adicona switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 class App extends Component {
@@ -12,9 +12,11 @@ class App extends Component {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning"/>}/>
+        <Switch>
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning"/>}/>
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     );
   }
